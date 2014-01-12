eda193fa8f7d06ca60b35e71aac4950f3ff76ad5
MrPoxipol <michal.proszek@gmail.com>
2014-01-12 16:34:57 +0100

    Rotation for sprite added.

--- js/engine.js
+++ js/engine.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,34 +12,46 @@ function Rect(pos, size, fillStyle) {
     this.fillStyle = fillStyle;
 
     this.draw = function(ctx) {
-            ctx.fillStyle = this.fillStyle;
-            ctx.fillRect(pos.x, pos.y, size.x, size.y);
+        ctx.fillStyle = this.fillStyle;
+        ctx.fillRect(pos.x, pos.y, size.x, size.y);
     };
 }
 
 // Sprite Class
 function Sprite(imgSrc, pos) {
-    if(typeof(pos)==='undefined')
+    if (typeof (pos) === 'undefined')
         this.pos = new Vector2D(0, 0);
     else
         this.pos = pos;
-    
+
     this.imgSrc = imgSrc;
     this.img = new Image();
     this.img.src = imgSrc;
     this.sc = new Vector2D(1, 1);
+    this.rt = 0;
 
     this.draw = function(ctx) {
-            ctx.drawImage(this.img, this.pos.x, this.pos.y, this.sc.x*this.img.width, this.sc.y*this.img.height);
+        ctx.translate(this.pos.x + this.img.width / 2, this.pos.y + this.img.height / 2);
+        ctx.rotate(this.rt);
+        ctx.drawImage(this.img, this.img.width / 2 * (-1), this.img.height / 2 * (-1), this.img.width, this.img.height);
+        // reset canvas
+        ctx.rotate(this.rt * (-1));
+        ctx.translate((this.pos.x + this.img.width / 2) * (-1), (this.pos.y + this.img.height / 2) * (-1));
     };
-
     this.scale = function(sc) {
-            this.sc = sc;
+        this.sc = sc;
+    };
+    this.rotate = function(rotation) {
+        this.rt += rotation * Math.PI / 180;
+    };
+    this.getSize = function() {
+        var size = new Vector2D(this.img.width, this.img.height);
+        return size;
     };
 }
 
 // Text Class
-function Text(string, font, color, size, pos){
+function Text(string, font, color, size, pos) {
     this.string = string;
     this.font = size + "px " + font;
     this.color = color;
@@ -48,21 +60,21 @@ function Text(string, font, color, size, pos){
     this.pos.y += size;
 
     this.draw = function(ctx) {
-            ctx.font = this.font;
-            ctx.fillStyle = this.color;
-            ctx.fillText(this.string, this.pos.x, this.pos.y);
+        ctx.font = this.font;
+        ctx.fillStyle = this.color;
+        ctx.fillText(this.string, this.pos.x, this.pos.y);
     };
 }
 // Utiles
 function Vector2D(x, y) {
     this.x = x;
     this.y = y;
-    
-    this.minus = function(a) {
+
+    this.sub = function(a) {
         this.x -= a.x;
         this.y -= a.y;
     };
-    this.plus = function(a) {
+    this.add = function(a) {
         this.x += a.x;
         this.y += a.y;
     };
@@ -85,8 +97,8 @@ function Rect(pos, size, fillStyle) {
     this.fillStyle = fillStyle;
 
     this.draw = function(ctx) {
-            ctx.fillStyle = this.fillStyle;
-            ctx.fillRect(pos.x, pos.y, size.x, size.y);
+        ctx.fillStyle = this.fillStyle;
+        ctx.fillRect(pos.x, pos.y, size.x, size.y);
     };
 }
 
@@ -95,13 +107,17 @@ function Clock() {
     // Methods
     this.getElapsedTime = function() {
         var act_date = new Date();
-        
+
         return act_date - this.startTime;
     };
     this.reset = function() {
         var elapsed = this.getElapsedTime();
         this.startTime = new Date();
-        
+
         return elapsed;
     };
+}
+// Random
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -17,50 +17,102 @@ $(document).ready(function() {
 
     var g = {
         // GAME
-        spawns: new Array(new Vector3D(680, 361, 90), new Vector3D(25, 364, 0)), // template: x, y, rotation
+        spawns: new Array(new Vector3D(711, 361, 90), new Vector3D(15, 308, 0)), // template: x, y, rotation
         targets_act: new Array(),
-        
         gen_clock: new Clock(),
         gen_time: 1000, // ms
-        
+
         /* TEXTS */
         text: {
             header: new Text("Pony Hunter", "Arial Black", "black", 24, new Vector2D(10, 10))
         },
         gfx: {
-            bg: new Sprite("img/bg.png", new Vector2D(0, 0))
+            bg: new Sprite("img/bg.png", new Vector2D(0, 0)),
+            house: new Sprite("img/house.png", new Vector2D(490, 185)),
+            tree: new Sprite("img/tree.png", new Vector2D(64, 256)),
+            bushes: new Sprite("img/bushes.png", new Vector2D(-71, 343))
         },
         audio: {
             shot: new Audio("audio/shots/shotgun.wav")
         }
     };
 
-    function KillTarget(spawn_id){
-        this.sprite = new Sprite("img/pony.png");
-        this.spawn_id = spawn_id;
-        
-        var spawn_pos = g.spawns[spawn_id];
-        this.sprite.pos = new Vector2D(spawn_pos.x, spawn_pos.y + this.sprite.getSize().y);
-    }
-    // pony :D
-
     var user = {
         shoted: false
     };
+    
+    function KillTarget(spawn_id) {
+        this.sprite = new Sprite("img/pony.png");
+        this.spawn_id = spawn_id;
+        this.sprite.pos = new Vector2D(g.spawns[spawn_id].x, g.spawns[spawn_id].y);
+        this.sprite.rotate(g.spawns[spawn_id].z);
+    }
+    // pony :D
     // Events
-    function onClick(e){
+    function onClick(e) {
         var position = $(c.name).position();
-        var x = e.pageX-position.left;
-        var y = e.pageY-position.top;
+        var x = e.pageX - position.left;
+        var y = e.pageY - position.top;
 
-        if(g.audio.shot.ended || !user.shoted)
+        if (g.audio.shot.ended || !user.shoted)
         {
             g.audio.shot.play();
             user.shoted = true;
         }
     }
 
-    function init(){
+    function getKillTargetOnSpawn(spawn_id) {
+        for (i = 0; i < g.targets_act.length; ++i)
+            if (g.targets_act[i].spawn_id === spawn_id)
+                return i;
+
+        return -1;
+    }
+
+    function generateKillTarget() {
+        if (g.targets_act.length >= g.spawns.length)
+            return false;
+
+        var spawn_id = getRandomInt(0, g.spawns.length - 1);
+        // Checking, is any killtarget on this spawn.
+        if (getKillTargetOnSpawn(spawn_id) !== -1)
+            return false;
+
+        g.targets_act.push(new KillTarget(spawn_id));
+
+        return true;
+    }
+
+    function drawKillTargets() {
+        for (i = 0; i < g.targets_act.length; ++i)
+            g.targets_act[i].sprite.draw(c.ctx);
+    }
+    
+    function drawScene() {
+        g.gfx.house.draw(c.ctx);
+        g.gfx.bushes.draw(c.ctx);
+        g.gfx.tree.draw(c.ctx);
+    }
+
+    function logic() {
+        if (g.gen_clock.getElapsedTime() >= g.gen_time) {
+            generateKillTarget();
+            g.gen_clock.reset();
+        }
+    }
+
+    function paint() {
+        logic();
+        c.ctx.fillStyle = "#000";
+        c.ctx.fillRect(0, 0, c.w, c.h);
+        // Drawing content
+        g.gfx.bg.draw(c.ctx);
+        drawKillTargets();
+        drawScene();
+        g.text.header.draw(c.ctx);
+    }
+
+    function init() {
         // Canvas init
         c.document.style.cursor = "url('img/cursor.png'), default";
 
@@ -71,48 +123,9 @@ $(document).ready(function() {
         c.h = $(c.name).height();
         // Events init
         c.canvas.addEventListener('click', onClick, false);
-        
+
         setInterval(paint, c.frameTime);
     }
-
-    function getKillTargetOnSpawn(spawn_id) {
-        for(i = 0; i < g.targets_act.length; ++i) {
-            if(g.targets_act.spawn_id == spawn_id)
-                return i;
-        }
-        
-        return -1;
-    }
-
-    function generateKillTarget() {
-        if(g.targets_act.length >= g.spawns.length)
-            return false;
-       
-        var spawn_id = Maths.random() % g.spawns.length;
-        // Checking, is any killtarget on this spawn.
-        if(getKillTargetOnSpawn(spawn_id) != -1)
-            return false;
-        
-        g.targets_act.push(new KillTarget(spawn_id));
-        
-        return true;
-    }
-
-    function logic(){
-        if(g.gen_clock.getElapsedTime() >= g.gen_time){
-            generateKillTarget();
-            g.gen_clock.reset();
-        }
-    }
-
-    function paint(){
-        logic();
-        c.ctx.fillStyle = "#000";
-        c.ctx.fillRect(0, 0, c.w, c.h);
-        // Drawing content
-        g.gfx.bg.draw(c.ctx);
-        g.text.header.draw(c.ctx);
-    }
-
+    
     init();
 });
